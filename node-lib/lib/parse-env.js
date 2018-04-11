731bcc7626626161225b46f4669966e8592dd211
Glenn Rempe <glenn@tierion.com>
2018-04-10 17:55:09 -0700

    Min Node version 1.3.7

--- node-lib/lib/parse-env.js
+++ node-lib/lib/parse-env.js
@@ -90,8 +90,8 @@ let envDefinitions = {
 
   // Redis related variables
   REDIS_CONNECT_URI: envalid.url({ devDefault: 'redis://redis:6379', desc: 'The Redis server connection URI' }),
-  MIN_NODE_VERSION_EXISTING: envalid.str({ default: '1.3.6', desc: 'The minimum acceptable Node version for existing registrations and to pass audits' }),
-  MIN_NODE_VERSION_NEW: envalid.str({ default: '1.3.7', desc: 'The minimum acceptable Node version for new registrations' }),
+  MIN_NODE_VERSION_EXISTING: envalid.str({ default: '1.3.7', desc: 'The minimum acceptable Node version for existing registrations and to pass audits' }),
+  MIN_NODE_VERSION_NEW: envalid.str({ default: '1.3.8', desc: 'The minimum acceptable Node version for new registrations' }),
 
   // Service Specific Variables
 
