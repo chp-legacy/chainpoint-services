cc6866cd4c4d3ae7ed4f123fd1e63e1d28420fa3
Jason Bukowski <jasonbukowski@users.noreply.github.com>
2018-05-30 14:59:01 -0400

    Migrate from web3 to ethers.js

---
+++ node-eth-tnt-tx-service/lib/endpoints/tntFunctions.js
@@ -0,0 +1,186 @@
+/* Copyright (C) 2018 Tierion
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+const env = require('../parse-env.js')('eth-tnt-tx')
+const _ = require('lodash')
+const restify = require('restify')
+const ethers = require('ethers')
+const retry = require('async-retry')
+const tntDefinition = require('../eth-tnt/TierionNetworkToken.json')
+
+// This value is set once the connection has been established
+let redis = null
+
+// This value is set by the updateETHGasPriceAsync function at regular intervals
+let recommendedGasPrice = null
+
+// Key for the Redis set of recent gas prices, the higher price over the
+// entire timeframe will be used
+const GAS_PRICE_HISTORY_KEY = 'ETH_TNT_Tx:GasPriceHistory'
+
+const network = env.isProduction ? ethers.providers.networks.homestead : ethers.providers.networks.ropsten
+const tntSourceWalletPK = env.ETH_TNT_SOURCE_WALLET_PK
+const tntContractAddr = tntDefinition.networks[env.isProduction ? '1' : '3'].address
+const etherscanProvider = new ethers.providers.EtherscanProvider(network, env.ETH_ETHERSCAN_API_KEY)
+const infuraProvider = new ethers.providers.InfuraProvider(network, env.ETH_INFURA_API_KEY)
+const fallbackProvider = new ethers.providers.FallbackProvider([infuraProvider, etherscanProvider])
+const readContract = new ethers.Contract(tntContractAddr, tntDefinition.abi, infuraProvider)
+const wallet = new ethers.Wallet(tntSourceWalletPK, fallbackProvider)
+const writeContract = new ethers.Contract(tntContractAddr, tntDefinition.abi, wallet)
+
+// validate addresses are individually well formed
+let isEthereumAddr = (address) => {
+  return /^0x[0-9a-f]{40}$/i.test(address)
+}
+
+async function updateETHGasPriceAsync () {
+  // Price history is kept in a sorted set with the score set to the timestamp
+  // On each add to this set, we prune element from the set older than 1 hour
+  // The highest price in the set is used for the next transaction
+  // Doing so guards against momentary dips in price estimation that may result
+  // in long processing times for transactions.
+  try {
+    let currentTimestamp = Date.now()
+    let newExpireTimestamp = currentTimestamp + 60 * 60 * 1000 // one hour from now
+    // Get the latest gas price recommendation
+    let currentGasPrice = await fallbackProvider.getGasPrice()
+    // Add lastest gas price to sorted set
+    await redis.zadd(GAS_PRICE_HISTORY_KEY, newExpireTimestamp, currentGasPrice.toString())
+    // Prune sorted set items older than 1 hour
+    await redis.zremrangebyscore(GAS_PRICE_HISTORY_KEY, '-inf', currentTimestamp)
+    // Retrieve the entire set and find the largest value
+    let prices = await redis.zrangebyscore(GAS_PRICE_HISTORY_KEY, currentTimestamp, '+inf')
+    // find the highest price in that set and update recommendedGasPrice
+    prices = prices.map((price) => parseInt(price))
+    recommendedGasPrice = Math.max(...prices)
+    // liberally increase this value by 50% to maximize confirmation speed
+    recommendedGasPrice *= 1.5
+    // enforce a minimum of 10 Gwei
+    recommendedGasPrice = Math.max(recommendedGasPrice, 10000000000)
+    console.log(`New price received : ${currentGasPrice} : Updated recommended price : ${recommendedGasPrice}`)
+  } catch (error) {
+    console.error(`ERROR : Could not update ETH recommended gas price : ${error.message}`)
+  }
+}
+
+async function getBalanceByTNTAddrV1Async (req, res, next) {
+  // Verify address
+  if (!req.params.hasOwnProperty('tnt_addr')) {
+    return next(new restify.InvalidArgumentError('invalid JSON body, missing tnt_addr'))
+  }
+
+  if (_.isEmpty(req.params.tnt_addr)) {
+    return next(new restify.InvalidArgumentError('invalid JSON body, empty tnt_addr'))
+  }
+
+  // use local wallet address when /balance/wallet requested
+  let tntTargetAddress = req.params.tnt_addr
+  if (tntTargetAddress === 'wallet') tntTargetAddress = wallet.address
+
+  if (!tntTargetAddress.startsWith('0x')) tntTargetAddress = `0x${tntTargetAddress}`
+
+  if (!isEthereumAddr(tntTargetAddress)) {
+    return next(new restify.InvalidArgumentError('invalid JSON body, malformed tnt_addr'))
+  }
+
+  try {
+    await retry(async bail => {
+      let grainsBalance = await readContract.balanceOf(tntTargetAddress)
+      let grainsBalanceString = grainsBalance.toString()
+      let tntBalance = grainsBalance.div(10 ** 8)
+      let tntBalanceString = tntBalance.toString()
+
+      res.send({
+        balance: grainsBalanceString
+      })
+
+      console.log(`Balance requested for ${tntTargetAddress}: ${grainsBalanceString} grains (${tntBalanceString} TNT)`)
+
+      return next()
+    }, {
+      retries: 10,        // The maximum amount of times to retry the operation. Default is 10
+      factor: 1,        // The exponential factor to use. Default is 2
+      minTimeout: 100,   // The number of milliseconds before starting the first retry. Default is 1000
+      maxTimeout: 1000,
+      onRetry: (error) => { console.error(`Transfer error : ${error.message}`) }
+    })
+  } catch (error) {
+    // if we get this far, transfer has failed, return an error
+    return next(new restify.InternalServerError('server error on balance check'))
+  }
+}
+
+async function postTransferV1Async (req, res, next) {
+  if (req.contentType() !== 'application/json') {
+    return next(new restify.InvalidArgumentError('invalid content type'))
+  }
+
+  // Verify address
+  if (!req.params.hasOwnProperty('to_addr')) {
+    return next(new restify.InvalidArgumentError('invalid JSON body, missing to_addr'))
+  }
+
+  if (_.isEmpty(req.params.to_addr)) {
+    return next(new restify.InvalidArgumentError('invalid JSON body, empty to_addr'))
+  }
+
+  if (!isEthereumAddr(req.params.to_addr)) {
+    return next(new restify.InvalidArgumentError('invalid JSON body, malformed to_addr'))
+  }
+
+  // Verify value
+  if (!req.params.hasOwnProperty('value')) {
+    return next(new restify.InvalidArgumentError('invalid JSON body, missing \'value\''))
+  }
+
+  let grains = parseInt(req.params.value)
+  if (_.isNaN(grains)) {
+    return next(new restify.InvalidArgumentError('invalid number specified for \'value\''))
+  }
+
+  try {
+    await retry(async bail => {
+      let tx = await writeContract.transfer(req.params.to_addr, grains, {
+        gasLimit: 50000,
+        gasPrice: recommendedGasPrice || 10000000000 // Use 10 Gwei as default if recommendedGasPrice is not set
+      })
+
+      console.log(`Transfered TNT to ${req.params.to_addr} : ${grains} grains (${ethers.utils.bigNumberify(grains).div(10 ** 8)} TNT) : TX INFO : ${JSON.stringify(tx)}`)
+
+      res.send({
+        trx_id: tx.hash
+      })
+
+      return next()
+    }, {
+      retries: 10,        // The maximum amount of times to retry the operation. Default is 10
+      factor: 1,        // The exponential factor to use. Default is 2
+      minTimeout: 100,   // The number of milliseconds before starting the first retry. Default is 1000
+      maxTimeout: 1000,
+      onRetry: (error) => { console.error(`Transfer error : ${error.message}`) }
+    })
+  } catch (error) {
+    // if we get this far, transfer has failed, return an error
+    return next(new restify.InternalServerError('server error on transfer'))
+  }
+}
+
+module.exports = {
+  getBalanceByTNTAddrV1Async: getBalanceByTNTAddrV1Async,
+  postTransferV1Async: postTransferV1Async,
+  updateETHGasPriceAsync: updateETHGasPriceAsync,
+  setRedis: (r) => { redis = r }
+}
--- node-eth-tnt-tx-service/server.js
+++ node-eth-tnt-tx-service/server.js
@@ -18,20 +18,8 @@
 const env = require('./lib/parse-env.js')('eth-tnt-tx')
 
 const restify = require('restify')
-const loadProvider = require('./lib/eth-tnt/providerLoader.js')
-const loadToken = require('./lib/eth-tnt/tokenLoader.js')
-const TokenOps = require('./lib/eth-tnt/tokenOps.js')
-const _ = require('lodash')
-var Web3 = require('web3')
-const retry = require('async-retry')
-const rp = require('request-promise-native')
-const keccak256 = require('js-sha3').keccak256
 const connections = require('./lib/connections.js')
-
-// The provider, token contract, and create the TokenOps class
-let web3Provider = null
-let tokenContract = null
-let ops = null
+const tntFunctions = require('./lib/endpoints/tntFunctions.js')
 
 // RESTIFY SETUP
 // 'version' : all routes will default to this version
@@ -56,215 +44,47 @@ server.use(restify.bodyParser({
 
 // API RESOURCES
 
-// validate addresses are individually well formed
-let isEthereumAddr = (address) => {
-  return /^0x[0-9a-fA-F]{40}$/i.test(address)
-}
-
-async function getBalanceFromInfuraAsync (tntAddr) {
-  // Hex encoding needs to start with 0x.
-  // First comes the function selector, which is the first 4 bytes of the
-  // keccak256 hash of the function signature.
-  // ABI-encoded arguments follow. The address must be left-padded to 32 bytes.
-  let functionSelectorHex = keccak256.hex('balanceOf(address)').substr(0, 8)
-  let tntAddrNo0x = tntAddr.substr(2) // chop off the 0x
-  let requestDataHexString = `0x${functionSelectorHex}000000000000000000000000${tntAddrNo0x}`
-
-  let options = {
-    headers: [
-      {
-        name: 'Content-Type',
-        value: 'application/json'
-      }
-    ],
-    method: 'POST',
-    uri: env.ETH_PROVIDER_URI,
-    body: {
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'eth_call',
-      params: [{
-        to: env.ETH_TNT_TOKEN_ADDR,
-        data: requestDataHexString
-      }, 'latest']
-    },
-    json: true,
-    gzip: true,
-    timeout: 1250,
-    resolveWithFullResponse: true
-  }
-
-  let balanceGrains
-  try {
-    await retry(async bail => {
-      let balanceResponse = await rp(options)
-      balanceGrains = parseInt(balanceResponse.body.result, 16)
-      if (isNaN(balanceGrains)) {
-        let errorMessage = `balanceGrains is NaN`
-        throw errorMessage
-      }
-    }, {
-      retries: 3,    // The maximum amount of times to retry the operation. Default is 10
-      factor: 1,       // The exponential factor to use. Default is 2
-      minTimeout: 200,   // The number of milliseconds before starting the first retry. Default is 1000
-      maxTimeout: 400,
-      randomize: true
-    })
-  } catch (error) {
-    let errorMessage = `getBalanceFromInfuraAsync : ${error.message}`
-    throw errorMessage
-  }
-
-  return balanceGrains
-}
-
-async function getBalanceFromGethAsync (tntAddr) {
-  return new Promise((resolve, reject) => {
-    ops.getBalance(tntAddr, (error, grains) => {
-      if (error) return reject(error.message)
-      if (isNaN(grains)) {
-        let errorMessage = `getBalance is NaN`
-        return reject(errorMessage)
-      }
-      return resolve(grains)
-    })
-  })
-}
-
-// get the TNT balance of node in grains
-server.get({ path: '/balance/:tnt_addr/', version: '1.0.0' }, async (req, res, next) => {
-  // Verify address
-  if (!req.params.hasOwnProperty('tnt_addr')) {
-    return next(new restify.InvalidArgumentError('invalid JSON body, missing tnt_addr'))
-  }
-
-  if (_.isEmpty(req.params.tnt_addr)) {
-    return next(new restify.InvalidArgumentError('invalid JSON body, empty tnt_addr'))
-  }
-
-  // use local wallet address when /balance/wallet requested
-  let ethAddress = req.params.tnt_addr
-  try {
-    if (ethAddress === 'wallet') {
-      let ethWallet = JSON.parse(env.ETH_WALLET)
-      ethAddress = ethWallet.address
-    }
-  } catch (error) {
-    return next(new restify.InvalidArgumentError('invalid JSON body, malformed eth_wallet value'))
-  }
-  if (!ethAddress.startsWith('0x')) ethAddress = `0x${ethAddress}`
-
-  if (!isEthereumAddr(ethAddress)) {
-    return next(new restify.InvalidArgumentError('invalid JSON body, malformed tnt_addr'))
-  }
-
-  let gethOnly = false
-  if (req.headers && !_.isEmpty(req.headers['geth-only'])) {
-    gethOnly = true
-  }
-
-  let serviceUsed
-  let grainsBalance = null
-  if (!gethOnly) {
-    // get grainsBalance from Infura API
-    try {
-      grainsBalance = await getBalanceFromInfuraAsync(ethAddress)
-      serviceUsed = 'infura'
-    } catch (error) {
-      console.error(`Could not get balance from Infura for address ${ethAddress} : ${error}`)
-    }
-  }
-
-  // if Infura did not return a balance, retrieve from geth
-  if (grainsBalance === null) {
-    try {
-      grainsBalance = await getBalanceFromGethAsync(ethAddress)
-      serviceUsed = 'geth'
-    } catch (error) {
-      console.error(`Could not get balance from geth for address ${ethAddress} : ${error.message}`)
-    }
-  }
-
-  // if grainsBalance is still null, return an error
-  if (grainsBalance === null) return next(new restify.InternalServerError('server error'))
-
-  res.send({
-    balance: grainsBalance
-  })
-
-  console.log(`Balance requested for ${ethAddress}: ${grainsBalance} grains (${grainsBalance / 10 ** 8} TNT) : ${serviceUsed}`)
-
-  return next()
-})
-
+// get the TNT balance for a specific TNT address in grains
+server.get({ path: '/balance/:tnt_addr/', version: '1.0.0' }, tntFunctions.getBalanceByTNTAddrV1Async)
 // send TNT grains to an address
-server.post({ path: '/transfer/', version: '1.0.0' }, (req, res, next) => {
-  if (req.contentType() !== 'application/json') {
-    return next(new restify.InvalidArgumentError('invalid content type'))
-  }
+server.post({ path: '/transfer/', version: '1.0.0' }, tntFunctions.postTransferV1Async)
 
-  // Verify address
-  if (!req.params.hasOwnProperty('to_addr')) {
-    return next(new restify.InvalidArgumentError('invalid JSON body, missing to_addr'))
-  }
-
-  if (_.isEmpty(req.params.to_addr)) {
-    return next(new restify.InvalidArgumentError('invalid JSON body, empty to_addr'))
-  }
-
-  if (!isEthereumAddr(req.params.to_addr)) {
-    return next(new restify.InvalidArgumentError('invalid JSON body, malformed to_addr'))
-  }
-
-  // Verify value
-  if (!req.params.hasOwnProperty('value')) {
-    return next(new restify.InvalidArgumentError('invalid JSON body, missing \'value\''))
-  }
-
-  let grains = parseInt(req.params.value)
-  if (_.isNaN(grains)) {
-    return next(new restify.InvalidArgumentError('invalid number specified for \'value\''))
-  }
-
-  ops.sendTokens(req.params.to_addr, grains, (error, result) => {
-    // Check for error
-    if (error) {
-      console.error(error)
-      return next(new restify.InternalServerError('server error'))
-    }
-
-    res.send({
-      trx_id: result
+/**
+ * Opens a Redis connection
+ *
+ * @param {string} redisURI - The connection string for the Redis instance, an Redis URI
+ */
+function openRedisConnection (redisURIs) {
+  connections.openRedisConnection(redisURIs,
+    (newRedis) => {
+      tntFunctions.setRedis(newRedis)
+    }, () => {
+      tntFunctions.setRedis(null)
+      setTimeout(() => { openRedisConnection(redisURIs) }, 5000)
     })
+}
 
-    console.log(`Transfered TNT to ${req.params.to_addr} : ${grains} grains (${grains / 10 ** 8} TNT) : TX ID : ${result}`)
-
-    return next()
-  })
-})
+function startIntervals () {
+  let intervals = [{
+    function: () => {
+      tntFunctions.updateETHGasPriceAsync()
+    },
+    immediate: true, // run this once immediately
+    ms: 600000 // every ten minutes
+  }]
+  connections.startIntervals(intervals)
+}
 
 // process all steps need to start the application
 async function start () {
   if (env.NODE_ENV === 'test') return
   try {
-    // Init the web3 provider
-    web3Provider = loadProvider(env.ETH_PROVIDER_URI)
-    // Set the default account to use for outgoing trxs
-    let web3 = new Web3(web3Provider)
-    web3.eth.getAccounts((error, accounts) => {
-      if (error) {
-        console.error(error)
-      }
-      web3.eth.defaultAccount = accounts[0]
-    })
-
-    // Load the token object
-    tokenContract = await loadToken(web3Provider, env.ETH_TNT_TOKEN_ADDR)
-    ops = new TokenOps(tokenContract)
-
+    // init Redis
+    openRedisConnection(env.REDIS_CONNECT_URIS)
     // Init Restify
     await connections.listenRestifyAsync(server, env.LISTEN_TX_PORT)
-
+    // init interval functions
+    startIntervals()
     console.log('startup completed successfully')
   } catch (error) {
     console.error(`An error has occurred on startup: ${error.message}`)
@@ -273,7 +93,4 @@ async function start () {
 }
 
 // get the whole show started
-setTimeout(start, 10000)
-
-// export these functions for unit tests
-module.exports = {}
+start()
